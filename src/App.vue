e7a300cc5a27c01153b84d5ed25ab7d587085fda
David Amegah <amegah40@gmail.com>
2022-11-02 02:13:55 +0000

    complete basic app

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,96 @@
 <template>
-  <div>
-    <h1 v-html="this.question"></h1>
 
+  <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
+
+  <div v-if="this.question">
+    <h1 v-html="this.question"></h1>
+    <template v-for="(answer, index) in this.answers" :key="index">
+      <input 
+        type="radio"
+        name="options"
+        :value="answer"
+        v-model="this.chosenAnswer" 
+        :disabled="this.answerSubmitted"
+      >
+      <label for="options" v-html="answer"></label>
+      <br>
+    </template>
+
+    <button v-if="!this.answerSubmitted" class="send" type="button" @click="submitAnswer()">Send</button>
+
+    <section class="result" v-if="this.answerSubmitted">
+      <h4 v-if="this.chosenAnswer == this.correctAnswer">
+        &#9989; Congratulations, the answer "{{ this.correctAnswer }}" is correct!
+      </h4>
+      <h4 v-else>
+        &#10060; Sorry, your answer is incorrect, the correct answer is "{{ this.correctAnswer }}"
+      </h4>
+      <button @click="getNewQuestion()" type="button">Next Question</button>
+    </section>
   </div>
 </template>
 
 <script>
 const API = "https://opentdb.com/api.php?amount=1";
+import ScoreBoard from ".//components/ScoreBoard.vue";
 
 export default {
   name: 'App',
+
+  components: {
+    ScoreBoard
+  },
+
   methods: {
-    getQuiz() {}
+    submitAnswer() {
+      if(!this.chosenAnswer){
+        alert("Please Pick one answer")
+      }else{
+        this.answerSubmitted = true;
+        if (this.chosenAnswer == this.correctAnswer){
+          this.winCount++
+        }else{
+          this.loseCount++
+        }
+      }
+    },
+    getNewQuestion(){
+      this.answerSubmitted = false
+      this.question = undefined
+      this.chosenAnswer = undefined
+
+      this.axios.get(API).then((response) => {
+        // console.log(response.data.results)
+        var questionResults = response.data.results;
+        this.question = questionResults[0].question;
+        this.correctAnswer = questionResults[0].correct_answer;
+        this.inCorrectAnswers = questionResults[0].incorrect_answers;
+      })
+    }
   },
   data(){
     return{
       question: undefined,
       correctAnswer: undefined,
-      inCorrectAnswers: undefined
+      inCorrectAnswers: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0
     }
   },
 
   computed: {
     answers() {
       var answers = JSON.parse(JSON.stringify(this.inCorrectAnswers))
-      answers.push(this.correctAnswer)
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer)
 
       return answers;
     }
   },
 
   created() {
-    this.axios.get(API).then((response) => {
-      // console.log(response.data.results)
-      var questionResults = response.data.results;
-      this.question = questionResults[0].question;
-      this.correctAnswer = questionResults[0].correct_answer;
-      this.inCorrectAnswers = questionResults[0].incorrect_answers;
-    })
+    this.getNewQuestion()
   }
 }
 </script>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>Player 1 <h1>{{this.winCount}}</h1></h3>
+        <h3>Computer<h1>{{this.loseCount}}</h1></h3>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["winCount", "loseCount"]
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
